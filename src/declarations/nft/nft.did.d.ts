78aae9566ae9f54a81b614f79f8d6a9a6b6aabe6
evve0119 <58882040+evve0119@users.noreply.github.com>
2022-11-20 21:14:10 -0600

    1119 version

--- src/declarations/nft/nft.did.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export interface NFT {
-  'getAsset' : () => Promise<Array<number>>,
-  'getCanisterId' : () => Promise<Principal>,
-  'getName' : () => Promise<string>,
-  'getOwner' : () => Promise<Principal>,
-  'transferOwnership' : (arg_0: Principal) => Promise<string>,
-}
-export interface _SERVICE extends NFT {}
